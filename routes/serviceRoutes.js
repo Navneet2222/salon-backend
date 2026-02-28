93572f99cd5ef82e59e526e7188f6ecfb622b57e
Navneet Kumar Singh <navneetkumarsingh2828@gmail.com>
2026-03-01 01:38:02 +0530

    Initial commit: project files

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const serviceController = require('../controllers/serviceController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Protect the route to create a service
+// 1. Add a new service to a shop (Requires Owner Login)
 router.post('/', authMiddleware, serviceController.createService);
 
-// Make the route to view services public
+// 2. Fetch all services for a specific shop (Public - so customers can see them)
 router.get('/:shopId', serviceController.getServicesByShop);
 
 module.exports = router;
